993a73f6ca0a602fe24c9b4d56092a3e2a5ca2aa
Jisu Kim <indexkim33@gmail.com>
2022-05-11 08:47:30 +0900

    Revert "대화창 만들기"
    This reverts commit 2a62cd5f1db2a89305045c4643b03aac5787d842.

--- My project/Assets/Customer.cs
+++ My project/Assets/Customer.cs
@@ -8,25 +8,20 @@ public class Customer : MonoBehaviour
     Rigidbody2D rigidbody;
     public Sprite[] sprite; //이미지   0평소     1좋음     2나쁨
     public string[] comment; //대사 배열
-    //int satisfaction;   //만족도
+    int satisfaction;   //만족도
     //모델
-
 
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         rigidbody=GetComponent<Rigidbody2D>();
         rigidbody.velocity = Vector2.up*15;
-        //satisfaction = -1;
-
+        satisfaction = -1;
         
     }
     private void Update()
     {
-        if (transform.position.y > -2)
-        {
-            rigidbody.velocity = Vector2.zero;
-        }
+        if (transform.position.y > -2) rigidbody.velocity = Vector2.zero;
 
     }
 
@@ -45,22 +40,10 @@ public class Customer : MonoBehaviour
         //2. 금액 지불하기
         //3. 밑으로 사라지기 (객체 삭제)
 
-        if (satisfaction > 50)
-        {
-            spriteRenderer.sprite = sprite[1]; //만족
-            //만족스러운 금액 지불
-        }
-        else
-        {
-            spriteRenderer.sprite = sprite[2]; //불만족
-            //불만족스러운 금액 지불
-        } 
+        spriteRenderer.sprite = sprite[1];
         Invoke("MoveDown", 1f);
         Destroy(this.gameObject, 1.5f);
     }
 
-    public string order()
-    {
-        return comment[0];
-    }
+   
 }
